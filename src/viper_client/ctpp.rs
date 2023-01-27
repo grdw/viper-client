47e78280624b685d3881e968c40f9f5b6b942939
grdw <gerard@grdw.nl>
2023-01-27 15:11:26 +0100

    WIP

--- src/viper_client/ctpp.rs
+++ src/viper_client/ctpp.rs
@@ -1,47 +1,97 @@
 use super::Command;
+use rand::Rng;
+use rand::distributions::{Distribution, Uniform};
+
+const R2_PREFIX: [u8; 2] = [64, 24];
+const R2_SUFFIX: [u8; 6] = [0, 3, 0, 14, 0, 0];
+const R3_SUFFIX: [u8; 10] = [0, 8, 0, 3, 73, 0, 39, 0, 0, 0];
 
 #[derive(Debug)]
 pub struct CTPP {
-    control: [u8; 3]
+    control: [u8; 3],
+    bitmask: Vec<u8>
 }
 
 impl CTPP {
     pub fn new(control: [u8; 3]) -> CTPP {
         CTPP {
-            control: control
+            control: control,
+            bitmask: CTPP::generate_mask()
         }
     }
 
-    fn r1(&self, actuator: &String, other_actuator: &String) -> Vec<u8> {
-        let start = [0, 24];
-        // TODO: What are these 4 bytes
-        // And why do they change every once in a while, it happens
-        // at r3() and r4()
-        let four = [0, 0, 0, 0];
-        // TODO: Also why do I stuff like such. Why 4x 255?
-        let stuff = [0, 0, 255, 255, 255, 255];
-        let link = format!("{}\0{}", actuator, other_actuator);
+    fn generate_mask() -> Vec<u8> {
+        let mut rng = rand::thread_rng();
+        let die = Uniform::from(1..255);
+
+        (0..4)
+            .map(|_| die.sample(&mut rng))
+            .collect::<Vec<u8>>()
+    }
+
+    fn r1(&self,
+          actuator: &String,
+          other_actuator: &String) -> Vec<u8> {
+
+        let prefix = [192, 24];
+        let suffix = [0, 16, 14, 0, 0];
 
         let req = [
-            &start[..],
-            &four[..],
+            &prefix[..],
+            &self.bitmask,
+            &actuator.as_bytes(),
+            &suffix[..]
+        ].concat();
+
+        return self.template(&req, actuator, other_actuator)
+    }
+
+    fn r2(&mut self,
+          actuator: &String,
+          other_actuator: &String) -> Vec<u8> {
+
+        let mask = CTPP::generate_mask();
+
+        let req = [
+            &R2_PREFIX[..],
+            &mask[..],
+            &R2_SUFFIX[..]
+        ].concat();
+
+        return self.template(&req, actuator, other_actuator)
+    }
+
+    fn r3(&mut self,
+          actuator: &String,
+          other_actuator: &String) -> Vec<u8> {
+
+        let mask = CTPP::generate_mask();
+
+        let req = [
+            &R2_PREFIX[..],
+            &mask[..],
+            &R3_SUFFIX[..]
+        ].concat();
+
+        return self.template(&req, actuator, other_actuator)
+    }
+
+    // All the CTPP requests follow the same template:
+    fn template(&self,
+                prefix: &[u8],
+                actuator: &String,
+                other_actuator: &String) -> Vec<u8> {
+
+        let stuff = [255, 255, 255, 255];
+        let link = format!("{}\0{}\0\0", actuator, other_actuator);
+
+        let req = [
+            &prefix[..],
             &stuff[..],
             &link.as_bytes()
         ].concat();
 
         Command::make(&req, &self.control)
-    }
-
-    fn r2(&self) {
-        let start = [64, 24];
-    }
-
-    fn r3(&self) {
-        let start = [96, 24];
-    }
-
-    fn r4(&self) {
-        let start = [192, 24];
     }
 }
 
@@ -51,14 +101,29 @@ mod tests {
     use std::str;
 
     #[test]
-    fn test_r1() {
-        let ctpp = CTPP { control: [1, 2, 3] };
-        let r1 = ctpp.r1(
+    fn test_template() {
+        let ctpp = CTPP::new([1, 2, 3]);
+        let t = ctpp.template(
+            &[0, 0, 0, 0, 0, 0, 0, 0],
             &String::from("SB0000062"),
             &String::from("SB000006")
         );
 
-        assert_eq!(str::from_utf8(&r1[20..29]).unwrap(), "SB0000062");
-        assert_eq!(str::from_utf8(&r1[30..38]).unwrap(), "SB000006");
+        assert_eq!(&t[16..20], &[255, 255, 255, 255]);
+        assert_eq!(str::from_utf8(&t[20..29]).unwrap(), "SB0000062");
+        assert_eq!(str::from_utf8(&t[30..38]).unwrap(), "SB000006");
+        assert_eq!(&t[29], &0);
+        assert_eq!(&t[38..40], &[0, 0]);
+    }
+
+    #[test]
+    fn test_r2() {
+        let mut ctpp = CTPP::new([1, 2, 3]);
+        let r2 = ctpp.r2(
+            &String::from("SB0000062"),
+            &String::from("SB000006")
+        );
+
+        assert_eq!(r2.len(), 44)
     }
 }
