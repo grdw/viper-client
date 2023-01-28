fde2660ba221266bf86fd2dc44336f76c7cc3d33
grdw <gerard@grdw.nl>
2023-01-28 10:49:11 +0100

    Re-enable some tests

--- src/viper_client.rs
+++ src/viper_client.rs
@@ -82,120 +82,85 @@ impl ViperClient {
     }
 }
 
-//#[cfg(test)]
-//mod tests {
-//    use super::*;
-//    use std::str;
-//    use std::thread;
-//    use std::net::TcpListener;
-//
-//    #[test]
-//    fn test_execute() {
-//        let listener = TcpListener::bind("127.0.0.1:3333").unwrap();
-//        let mut client = ViperClient::new(
-//            &String::from("127.0.0.1"),
-//            &String::from("3333"),
-//            &String::from("ABCDEF")
-//        );
-//
-//        // This is the doorbell server essentially
-//        thread::spawn(move || {
-//            let length = 2;
-//            let (mut socket, _addr) = listener.accept().unwrap();
-//            let mut buf = [0; 1];
-//            socket.read(&mut buf).unwrap();
-//            socket.write(&[
-//                0, 0, length, 0, 0, 0, 0, 0,
-//                65, 65
-//            ]).unwrap();
-//        });
-//
-//        let response = client.execute(&[0]).unwrap();
-//        assert_eq!(str::from_utf8(&response).unwrap(), "AA");
-//    }
-//
-//    #[test]
-//    fn test_make_command() {
-//        let listener = TcpListener::bind("127.0.0.1:3334").unwrap();
-//        let mut client = ViperClient::new(
-//            &String::from("127.0.0.1"),
-//            &String::from("3334"),
-//            &String::from("ABCDEF")
-//        );
-//
-//        // This is the doorbell server essentially
-//        thread::spawn(move || {
-//            let (mut socket, _addr) = listener.accept().unwrap();
-//            let mut head = [0; 8];
-//            socket.read(&mut head).unwrap();
-//
-//            let bl = Command::buffer_length(head[2], head[3]);
-//            let mut buf = vec![0; bl];
-//            socket.read(&mut buf).unwrap();
-//            socket.write(&[&head, &buf[..]].concat()).unwrap();
-//        });
-//
-//        let pre = Command::preflight("UCFG", &client.control);
-//        let r = client.execute(&pre).unwrap();
-//        assert_eq!(&r[0..8], &[205, 171, 1,  0, 7, 0, 0, 0]);
-//    }
-//
-//    #[test]
-//    fn test_make_uat_command() {
-//        let listener = TcpListener::bind("127.0.0.1:3335").unwrap();
-//        let mut client = ViperClient::new(
-//            &String::from("127.0.0.1"),
-//            &String::from("3335"),
-//            &String::from("ABCDEF")
-//        );
-//
-//        // This is the doorbell server essentially
-//        thread::spawn(move || {
-//            let (mut socket, _addr) = listener.accept().unwrap();
-//            let mut head = [0; 8];
-//            socket.read(&mut head).unwrap();
-//
-//            let bl = Command::buffer_length(head[2], head[3]);
-//            let mut buf = vec![0; bl];
-//            socket.read(&mut buf).unwrap();
-//            socket.write(&[&head, &buf[..]].concat()).unwrap();
-//        });
-//
-//        let aut = Command::make(
-//            client.command_json("UAUT").as_bytes(),
-//            &client.control
-//        );
-//        let r = client.execute(&aut).unwrap();
-//        assert_eq!(r.len(), 83);
-//    }
-//
-//    #[test]
-//    fn test_ctpp() {
-//        let listener = TcpListener::bind("127.0.0.1:3336").unwrap();
-//        let mut client = ViperClient::new(
-//            &String::from("127.0.0.1"),
-//            &String::from("3336"),
-//            &String::from("ABCDEF")
-//        );
-//
-//        thread::spawn(move || {
-//            let (mut socket, _addr) = listener.accept().unwrap();
-//            let mut head = [0; 8];
-//            socket.read(&mut head).unwrap();
-//
-//            let bl = Command::buffer_length(head[2], head[3]);
-//            let mut buf = vec![0; bl];
-//            socket.read(&mut buf).unwrap();
-//            socket.write(&[&head, &buf[..]].concat()).unwrap();
-//        });
-//
-//        let data = r#"
-//            {
-//                "apt-address":"SB0000011",
-//                "apt-subaddress": 2
-//            }
-//        "#;
-//        let v: serde_json::Value = serde_json::from_str(data).unwrap();
-//        _ = client.ctpp(&v);
-//    }
-//}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::str;
+    use std::thread;
+    use std::net::TcpListener;
+
+    #[test]
+    fn test_execute() {
+        let listener = TcpListener::bind("127.0.0.1:3333").unwrap();
+        let mut client = ViperClient::new(
+            &String::from("127.0.0.1"),
+            &String::from("3333")
+        );
+
+        // This is the doorbell server essentially
+        thread::spawn(move || {
+            let length = 2;
+            let (mut socket, _addr) = listener.accept().unwrap();
+            let mut buf = [0; 1];
+            socket.read(&mut buf).unwrap();
+            socket.write(&[
+                0, 0, length, 0, 0, 0, 0, 0,
+                65, 65
+            ]).unwrap();
+        });
+
+        let response = client.execute(&[0]).unwrap();
+        assert_eq!(str::from_utf8(&response).unwrap(), "AA");
+    }
+
+    #[test]
+    fn test_make_command() {
+        let listener = TcpListener::bind("127.0.0.1:3334").unwrap();
+        let mut client = ViperClient::new(
+            &String::from("127.0.0.1"),
+            &String::from("3334")
+        );
+
+        // This is the doorbell server essentially
+        thread::spawn(move || {
+            let (mut socket, _addr) = listener.accept().unwrap();
+            let mut head = [0; 8];
+            socket.read(&mut head).unwrap();
+
+            let bl = Command::buffer_length(head[2], head[3]);
+            let mut buf = vec![0; bl];
+            socket.read(&mut buf).unwrap();
+            socket.write(&[&head, &buf[..]].concat()).unwrap();
+        });
+
+        let command = "UCFG".to_string();
+        let pre = Command::preflight(&command, &client.control);
+        let r = client.execute(&pre).unwrap();
+        assert_eq!(&r[0..8], &[205, 171, 1,  0, 7, 0, 0, 0]);
+    }
+
+    #[test]
+    fn test_make_uat_command() {
+        let listener = TcpListener::bind("127.0.0.1:3335").unwrap();
+        let mut client = ViperClient::new(
+            &String::from("127.0.0.1"),
+            &String::from("3335")
+        );
+
+        // This is the doorbell server essentially
+        thread::spawn(move || {
+            let (mut socket, _addr) = listener.accept().unwrap();
+            let mut head = [0; 8];
+            socket.read(&mut head).unwrap();
+
+            let bl = Command::buffer_length(head[2], head[3]);
+            let mut buf = vec![0; bl];
+            socket.read(&mut buf).unwrap();
+            socket.write(&[&head, &buf[..]].concat()).unwrap();
+        });
+
+        let aut = Command::for_kind(CommandKind::UAUT("ABCDEFG".to_string()), &[1,2,3]);
+        let r = client.execute(&aut).unwrap();
+        assert_eq!(r.len(), 83);
+    }
+}
