71fcafa3be2226c0c29272140f61cd1ec8fee27e
grdw <gerard@grdw.nl>
2023-02-04 10:16:53 +0100

    Limit the random byte generator to 0x80
    Part of #20

--- src/helper.rs
+++ src/helper.rs
@@ -1,11 +1,18 @@
 use rand::distributions::{Distribution, Uniform};
 
+const START: u8 = 0x01;
+
+// The reason why it stops at 0x80 is because in some parts of the
+// protocol the server adds 0x80 to the total, which could result in
+// a number higher than 0xff, which doesn't exist.
+const END: u8 = 0x80;
+
 pub struct Helper {}
 
 impl Helper {
     pub fn gen_ran(size: usize) -> Vec<u8> {
         let mut rng = rand::thread_rng();
-        let die = Uniform::from(1..253);
+        let die = Uniform::from(START..END);
 
         (0..size)
             .map(|_| die.sample(&mut rng))
@@ -14,7 +21,7 @@ impl Helper {
 
     pub fn control() -> [u8; 2] {
         let mut rng = rand::thread_rng();
-        let die = Uniform::from(1..253);
+        let die = Uniform::from(START..END);
 
         [
             die.sample(&mut rng),
